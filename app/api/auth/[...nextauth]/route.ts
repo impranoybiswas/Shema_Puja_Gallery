8b9c610a7f940b3b3b490126c8342c6928315f46
Pranoy Biswas <impranoybiswas@gmail.com>
2025-10-05 20:16:14 +0600

    Vercel Error Fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -51,7 +51,7 @@ export const authOptions: AuthOptions = {
         session.user.id = token.id as string;
         session.user.name = token.name as string;
         session.user.email = token.email as string;
-        session.user.role = token.role as string; // ✅ inject role into session.user
+        session.user.role = token.role as string;
       }
       return session;
     },
--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["ik.imagekit.io"],
   },
+  
 };
 
 export default nextConfig;
